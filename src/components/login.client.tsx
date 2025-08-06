a88191357f901f2beadd243bc326c6d8db3dc29c
Emilien ARNAUD <arnaud.emilien@chu-amiens.fr>
2025-08-06 15:40:46 +0200

    oidc keycloak implemented

---
+++ src/components/login.client.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import supabase from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+
+export function LoginTest() {
+  const router = useRouter();
+  return (
+    <button
+      className="border-2 border-black p-2"
+      type="button"
+      onClick={() =>
+        supabase.auth
+          .signInWithPassword({
+            email: "test@test.fr",
+            password: "test",
+          })
+          .then(({ data, error }) => {
+            if (data) {
+              console.log("Logged in", data);
+              router.refresh();
+            }
+            if (error) {
+              console.log("Error login", error.message);
+            }
+          })
+      }
+    >
+      Login as test@test.fr
+    </button>
+  );
+}
+
+export function LogoutButton() {
+  const router = useRouter();
+  return (
+    <button
+      className="border-2 border-black p-2"
+      type="button"
+      onClick={() => supabase.auth.signOut().then(() => router.refresh())}
+    >
+      Logout
+    </button>
+  );
+}
+
+export function KeycloakButton() {
+  const router = useRouter();
+  return (
+    <button
+      className="border-2 border-black p-2"
+      type="button"
+      onClick={() =>
+        supabase.auth
+          .signInWithOAuth({
+            provider: "keycloak",
+            options: { scopes: "openid profile" },
+          })
+          .then(({ data, error }) => {
+            if (data) {
+              console.log("Logged in", data);
+              router.refresh();
+            }
+            if (error) {
+              console.log("Error login", error.message);
+            }
+          })
+      }
+    >
+      Login with Keycloak
+    </button>
+  );
+}
